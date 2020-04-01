bf9376ec7d2e4876cf78e4ccd3ffc98a9423dd9b
Theo Auge <33973859+theoauge@users.noreply.github.com>
2020-04-01 15:19:29 -0500

    trying to figure out react

--- scripts/submit-entry.js
+++ scripts/submit-entry.js
@@ -1,33 +1,8 @@
-/*'use strict';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-const e = React.createElement;
-
-class LikeButton extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { liked: false };
-  }
-
-  render() {
-    if (this.state.liked) {
-      return 'You liked this.';
-    }
-
-    return e(
-      'button',
-      { onClick: () => this.setState({ liked: true }) },
-      'Like'
-    );
-  }
+function Hi() {
+  return <div>Hello World!</div>;
 }
 
-const domContainer = document.querySelector('#like_button_container');
-ReactDOM.render(e(LikeButton), domContainer);*/
-
-
-function myFunction() {  
-  ReactDOM.render(
-    <h1>Hello World</h1>,
-    document.getElementById('like_button_container')
-  );
-}
+ReactDOM.render(<Hi/>, document.querySelector('#root'));
